5444ae32045d7a58aaf54110c8e4016acea9c015
Santiago <tiagomorapaca@gmail.com>
2024-06-13 14:36:19 -0400

    Creacion de crud cajeros

---
+++ SistemaAlquilerDeComputadoras/Contexto/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SistemaAlquilerDeComputadoras.Models;
+
+namespace SistemaAlquilerDeComputadoras.Contexto
+{
+	public class MyContext: DbContext
+	{
+		public MyContext(DbContextOptions options) : base(options)
+		{
+		}
+		public DbSet<Usuario> Usuarios { get; set; }
+		public DbSet<Cliente> Clientes { get; set; }
+		public DbSet<Equipo> Equipos { get; set; }
+		public DbSet<Flete> Fletes { get; set; }
+	}
+}
