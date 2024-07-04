3430dd8e03b1de4bda5a13482d5880b47e36161f
Josi57 <josejohnaa@gmail.com>
2024-07-04 09:42:06 -0700

    Generar Recibo

--- SistemaAlquilerDeComputadoras/Controllers/FletesController.cs
+++ SistemaAlquilerDeComputadoras/Controllers/FletesController.cs
@@ -170,7 +170,56 @@ namespace SistemaAlquilerDeComputadoras.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        
+
+        // GET: Fletes/GenerarRecibo/5
+        public async Task<IActionResult> GenerarRecibo(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var flete = await _context.Fletes.FindAsync(id);
+            if (flete == null)
+            {
+                return NotFound();
+            }
+
+            var recibo = new Recibo
+            {
+                Flete = flete,
+                Cliente = new Cliente() 
+            };
+
+            return View("recibo", recibo); 
+        }
+
+        // POST: Fletes/GenerarRecibo/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> GenerarRecibo(int id, [Bind("Flete,Cliente")] Recibo recibo)
+        {
+            if (id != recibo.Flete.NroRecibo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+              
+                var clienteExistente = await _context.Clientes.FindAsync(recibo.Cliente.Ci);
+                if (clienteExistente == null)
+                {
+                    _context.Clientes.Add(recibo.Cliente);
+                    await _context.SaveChangesAsync();
+                }
+
+          
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View("recibo", recibo); 
+        }
 
     }
 }
---
+++ SistemaAlquilerDeComputadoras/Models/Recibo.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaAlquilerDeComputadoras.Models
+{
+    public class Recibo
+    {
+        public Flete? Flete { get; set; }
+        public Cliente? Cliente { get; set; }
+    }
+}
